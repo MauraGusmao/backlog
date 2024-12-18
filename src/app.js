52c1948c7650b96aca98f266aeda3c9ac7c2f0b4
Maura <137624881+MauraGusmao@users.noreply.github.com>
2024-12-18 20:20:09 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
- import express from "express";
- import bodyParser from "body-parser";
- import filmeRoutes from "../src/routes/filmeRoutes.js";
+import express from "express";//importação da lib
+import bodyParser from "body-parser";//importação da lib
+import filmeRoutes from "../src/routes/filmeRoutes.js";//importação do arquivo de rotas
 
- const app = express();
- 
- app.use(bodyParser.json());
+const app = express();//constante da lib
 
- app.use('/' ,filmeRoutes);
+app.use(bodyParser.json());//conversão para json
+app.use('/', filmeRoutes);//sempre que "/" for ativado, chama o arquivo filmeRoutes
 
- 
- export default app;
+export default app;//Exportando tudo nesse arquivo
