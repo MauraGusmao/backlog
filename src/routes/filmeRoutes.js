c1b45b0e5fa62f15321f968137c19629f5104362
Maura <137624881+MauraGusmao@users.noreply.github.com>
2024-12-18 20:19:40 -0300

    Update filmeRoutes.js
    função de deletar

--- src/routes/filmeRoutes.js
+++ src/routes/filmeRoutes.js
@@ -1,69 +1,54 @@
- import express from "express";
- import filmeController from "../controllers/filmeController.js";
-
- const router = express.Router();
- const filme = new filmeController();
-
- router.get('/' ,(req , res) =>{
-    try{
-        filme.BuscarTodosFilmes(req, res);
-    } 
-    catch(err){
-        res.status(500).json({erro: err.message})
-            
-    }
- });
-
- router.get('/:id', (req , res) =>{
-    try{
-        filme.BuscarFilmePorId(req , res);
-
-    }
-    catch(err){
-        res.status(500).json({erro: err.message});
-        
-    }
-
- });
-
-
- router.post('/' , (req , res)=> {
-
-    try{
-        filme.CadastrarFilme(req , res);
-
-        
-    }
-    catch(err){
-        res.status(500).json({erro: err.message});
-
-    }
-
- });
-
- router.put('/:id', (req , res) =>{
+import express from "express";
+import filmeController from "../controllers/filmeController.js";
+const router = express.Router();
+const filme = new filmeController();
+router.get('/', (req, res) => {
     try {
-        filme.AtualizarFilme(req , res);
-        
-    } catch (err ) {
-        
-        res.status(500).json({erro:err.message})
-    }
-
- });
-
- router.delete('/:id', (req , res)=>{
-    try {
-
-        filme.DeletarFilme(req ,res);
-        
+        filme.BuscarTodosOsFilmes(req, res);
     } catch (err) {
+        res.status(500).json({erro: err.message});
 
-        res.status(500).json({erro:err.message})
-        
     }
 
- });
+} );//Callback function, caso algo aconteça, executa a função
+
+router.post('/', (req, res) => {
+    try{
+      filme.CadastrarFilme(req, res);  
+    }
+    catch(err){
+        res.status(500).json({erro: err.message});
+
+    }}
+
+);
 
 
- export default router;
+router.get('/:id', (req, res) =>{//Pegando o parametro da rota
+    try{
+        const id = req.params.id;//Acessa o parametro da rota
+        filme.BuscarFilme(req, res, id);
+    }catch(err){
+        res.status(500).json({erro: err.message});
+    }
+});
+
+router.put("/:id", (req, res) =>{
+    try{
+        filme.AtualizarFilme(req, res);
+    }
+    catch(err){
+        res.status(500).json({erro: err.message});
+    }
+
+})
+
+router.delete("/:id", (req, res) =>{
+    try{
+        filme.DeletarFilme(req, res);
+    }catch(err){
+        res.status(500).json({erro:err.message});
+    }
+})
+
+export default router;
