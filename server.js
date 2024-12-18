857a750b73e6b433ef1afc64c08fc72daa51729f
Maura <137624881+MauraGusmao@users.noreply.github.com>
2024-12-18 20:20:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-import { query } from 'express';
-import app from './src/app.js';
+import app from './src/app.js';//Importando o objeto do arquivo app.js
 
-const PORT = 3001;
+const PORT = 3000;//porta padrão /servidor
 
-app.listen(PORT ,()=>{
-    console.log('servidor escutando')
-});
-
-
+app.listen(PORT, () => {
+    console.log("Servidor escutando");
+});//Usando o metodo listen, através de uma arrow function
