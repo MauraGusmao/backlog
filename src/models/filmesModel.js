e2b0d353f36b1f6fdb04770b0f4ea412ea5b52af
Maura <137624881+MauraGusmao@users.noreply.github.com>
2024-12-18 20:18:51 -0300

    Update filmesModel.js
    atualizando filme

--- src/models/filmesModel.js
+++ src/models/filmesModel.js
@@ -1,36 +1,31 @@
- import { DataTypes } from "sequelize";
-import Sequelize from "../config/dbConfig.js";
-
-const filmeModel = Sequelize.define('Filmes',{
+import { DataTypes } from "sequelize";
+import sequelize from "../config/dbConfig.js";
+//configurando sequelize
+const filmeModel = sequelize.define('filmes', {
     id: {
-        type : DataTypes.INTEGER,
+        type:DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: false
     },
     titulo: {
-        type : DataTypes.STRING,
-        allowNull: false
+        type:DataTypes.STRING,
+        allowNull: false,
     },
     diretor: {
-        type: DataTypes.STRING,
-        allowNull:false
-    },
-    ano: {
-        type : DataTypes.INTEGER,
+        type:DataTypes.STRING,
         allowNull: false
     },
-    gereno: {
-        type: DataTypes.STRING,
+    ano:{
+        type:DataTypes.INTEGER,
+        allowNull: false,
+    },
+    genero:{
+        type:DataTypes.STRING,
         allowNull: false
-
     }
-},   {
+}, {
     timestamps:false
-
-
-
 });
 
 export default filmeModel;
-    
